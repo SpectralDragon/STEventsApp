4423b9d0d018a6cdee98f3661d39edc2ba63cd74
Vladislav Prusakov <spectraldragon@bk.ru>
2017-10-21 03:03:45 +0300

    Initial Commit

---
+++ SberTech-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SberTech-Test
+//
+//  Created by Vladislav Prusakov on 21/10/2017.
+//  Copyright © 2017 Vladislav Prusakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SberTech-Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SberTech-Test
+//
+//  Created by Vladislav Prusakov on 21/10/2017.
+//  Copyright © 2017 Vladislav Prusakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
